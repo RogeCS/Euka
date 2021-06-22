676222f67d1b98353e3099370d058c88abfade40
Rogelio Medina <rogelio.medina.cs@gmail.com>
2021-06-22 09:18:21 -0500

    hide sidebar transition added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import Logo from "./Logo.jsx";
+import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "../code/SidebarData";
 
 import "../styles/components/Sidebar.scss";
 
 const Sidebar = () => {
+  const [sidebar, setSidebar] = React.useState(true);
+  const toggleSidebar = () => setSidebar(!sidebar);
+
   return (
-    <div className="dashboard-sidebar">
-      <div className="dashboard-sidebar__logo">
-        <Logo color="primary" />
+    <div
+      className={
+        sidebar
+          ? "dashboard-sidebar dashboard-sidebar--open"
+          : "dashboard-sidebar dashboard-sidebar--close"
+      }
+    >
+      <div className="dashboard-sidebar__toggle" onClick={toggleSidebar}>
+        <span className="dashboard-sidebar__close">
+          <AiIcons.AiFillCaretLeft />
+        </span>
+        <span className="dashboard-sidebar__open">
+          <AiIcons.AiFillCaretRight />
+        </span>
       </div>
+      <span className="dashboard-sidebar__logo">
+        <Logo color="primary" />
+      </span>
       <ul className="dashboard-sidebar__list">
         {SidebarData.map((val, key) => (
           <li
@@ -23,7 +41,7 @@ const Sidebar = () => {
               window.location.pathname = val.link;
             }}
           >
-            <div className="dashboard-sidebar__icon">{val.icon}</div>
+            <span className="dashboard-sidebar__icon">{val.icon}</span>
             <div
               className={`dashboard-sidebar__title ${
                 window.location.pathname == val.link
